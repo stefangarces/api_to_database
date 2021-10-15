bdb5c3dffdf467a491f5a74f8bd26f2382ecb1ac
Stefan Garcés <stefan.garces@alten.se>
2021-10-15 11:22:00 +0200

    Tried to add a "put" http request.

--- api_to_database/Controllers/Customers.cs
+++ api_to_database/Controllers/Customers.cs
@@ -47,6 +47,37 @@ namespace api_to_database.Controllers
             return CreatedAtRoute(nameof(Get), new { id = model.Id.ToString() }, model);
         }
 
+
+        [HttpPut("{socialSecurityNumber}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Put(PersonModelUpdateDTO PersonModelUpdateDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+
+            using (var ctx = new personDBtable())
+            {
+                var existingPerson = ctx.Students.Where(s => s.StudentID == student.Id)
+                                                        .FirstOrDefault<PersonModelUpdateDTO>();
+
+                if (existingPerson != null)
+                {
+                    existingPerson.FirstName = PersonModelUpdateDTO.Email;
+                    existingPerson.LastName = PersonModelUpdateDTO.SwishNumber;
+                    existingPerson.LastName = PersonModelUpdateDTO.Address;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return Ok();
+        }
+
         //[HttpPut]
         //[ProducesResponseType(StatusCodes.Status200OK)]
         //[ProducesResponseType(StatusCodes.Status404NotFound)]
